bdc42b5ac70e5af7cdbd50096c49576fbd8c3461
Munsterlandr <corgi47poke@gmail.com>
2022-02-22 20:38:02 -0700

    more skeleton

---
+++ src/main/kotlin/munsterlandr/handwraps/Handwraps.kt
@@ -0,0 +1,11 @@
+package munsterlandr.handwraps
+
+import net.fabricmc.api.ModInitializer
+
+object Handwraps: ModInitializer {
+    val MOD_ID = "handwraps"
+
+    override fun onInitialize() {
+
+    }
+}
